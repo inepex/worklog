1433e81b2853e8340d8d74729aa740ab13678f1a
tibor.hidi <tibor.hidi@8fbec55a-2e2f-4785-ad45-5398fcf08caf>
2013-03-01 13:39:35 +0000

    debug
    git-svn-id: https://svn.inepex.com/projects/Worklog@11534 8fbec55a-2e2f-4785-ad45-5398fcf08caf

--- classes/Log.php
+++ classes/Log.php
@@ -36,6 +36,7 @@ class Log{
 	public static function is_overlap($user_id ,$date, $from, $to,$log_id=""){
 		mysql_query("SELECT worklog_log_id FROM worklog_log WHERE worklog_user_id = '".$user_id."' AND log_date = '".$date."'");
 		debug(mysql_affected_rows());
+		debug("user_id".$user_id);
 // 		$condition = "";
 // 		if($log_id != "" && Log::is_log_exist($log_id)){
 // 			$condition = " AND worklog_log_id != ".$log_id;
