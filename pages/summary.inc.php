e5f6a2b552d54674dc48acfff31be2d2df1fd257
tibor.hidi <tibor.hidi@8fbec55a-2e2f-4785-ad45-5398fcf08caf>
2013-03-15 14:21:13 +0000

    git-svn-id: https://svn.inepex.com/projects/Worklog@11722 8fbec55a-2e2f-4785-ad45-5398fcf08caf

--- pages/summary.inc.php
+++ pages/summary.inc.php
@@ -70,7 +70,12 @@ if(isset($_GET['company_id']) && $_GET['company_id'] != "" && Company::is_compan
 						$date = new DateTime("now");
 						$date->modify("first day of this month");
 						if($date->format('Y-m-d') == $first_log_date->format('Y-m-d')){
-							echo '<option value="'.$date->format('Y-m-d').'" selected>'.$date->format('Y. F').'</option>';
+							if($selected_date == $date->format('Y-m-d')){
+								echo '<option value="'.$date->format('Y-m-d').'" selected>'.$date->format('Y. F').'</option>';
+							}
+							else{
+								echo '<option value="'.$date->format('Y-m-d').'" >'.$date->format('Y. F').'</option>';
+							}
 						}
 						else{
 							while($date->format('Y-m-d') != $first_log_date->format('Y-m-d')){
@@ -127,57 +132,57 @@ if(isset($_GET['company_id']) && $_GET['company_id'] != "" && Company::is_compan
 					google.load("visualization", "1", {packages:["corechart"]});
 					google.setOnLoadCallback(drawChart);
 					google.setOnLoadCallback(drawChart2);
-					
+
 					function drawChart() {
-					
+
 					var data = google.visualization.arrayToDataTable([
-					
+
 					["Type", "Count"],';
 					foreach ($user_worked_hours_in_categories as $user_worked_hours_in_category){
 						$category = new Category($user_worked_hours_in_category['category_id']);
 						echo '["'.$category->get_name().'", '.$user_worked_hours_in_category['worked_hours'].'],';
 					}
-						
+
 					echo ']
 					);
-					
+
 					var options = {
 					title: "user worked hours in category","width":400,
-                      "height":300,sliceVisibilityThreshold:0
-					};
-						
-					var chart = new google.visualization.PieChart(document.getElementById("chart_div'.$counter.'"));
-					chart.draw(data, options);
-					}
+					"height":300,sliceVisibilityThreshold:0
+				};
+
+				var chart = new google.visualization.PieChart(document.getElementById("chart_div'.$counter.'"));
+				chart.draw(data, options);
+				}
 					
 					
-					function drawChart2() {
+				function drawChart2() {
 					
-					var data2 = google.visualization.arrayToDataTable([
+				var data2 = google.visualization.arrayToDataTable([
 					
-					["Type", "Count"],';
+				["Type", "Count"],';
 					foreach ($user_worked_hours_in_projects as $user_worked_hours_in_project){
 						$project = new Project($user_worked_hours_in_project['project_id']);
 						echo '["'.$project->get_name().'", '.$user_worked_hours_in_project['worked_hours'].'],';
 					}
-						
+
 					echo ']
 					);
-					
+
 					var options2 = {
 					title: "user worked hours in projects","width":400,
-                      "height":300,sliceVisibilityThreshold:0
-					};
-						
-					var chart2 = new google.visualization.PieChart(document.getElementById("chart_div_'.$counter.'"));
-					chart2.draw(data2, options2);
-					}
-					</script>';
-					
+					"height":300,sliceVisibilityThreshold:0
+				};
+
+				var chart2 = new google.visualization.PieChart(document.getElementById("chart_div_'.$counter.'"));
+				chart2.draw(data2, options2);
+				}
+				</script>';
+
 					//
 					$counter++;
 				}
-				
+
 			}
 			?>
 		</table>
