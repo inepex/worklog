a16ff696c0379b4df1946a7e11b0d9c65302a0c9
tibor.hidi <tibor.hidi@8fbec55a-2e2f-4785-ad45-5398fcf08caf>
2013-03-27 12:27:16 +0000

    git-svn-id: https://svn.inepex.com/projects/Worklog@11971 8fbec55a-2e2f-4785-ad45-5398fcf08caf

--- test.php
+++ test.php
@@ -18,9 +18,13 @@ require_once 'classes/Log.php';
 require_once 'classes/ProjectStatus.php';
 require_once 'classes/WorkPlace.php';
 require_once 'include/notifications.php';
-echo "first: ".Log::get_first_log_date();
-$time = "12:34";
-echo $time;
-echo "<br>";
-var_dump(date_parse_from_format("H:i","24:00"));
+require_once 'classes/Tools.php';
+
+$text = "Ahhoz, hogy ezt a hírt a helyén kezeljük, tudni kell, hogy bár http://asdfís.hu az üvegszálas kábelekben a fény az, ami az információátvitelt lehetővé teszi, az információ sebessége meg sem közelíti a fény vákuumban mért sebességét. Míg az utóbbi nagyjából 300 000 kilométer per másodperc, a manapság széles körben alkalmazott, a légüres térnél jóval sűrűbb üvegszálas kábelekben leadott fényimpulzusok csak 200 000 kilométert tesznek meg másodpercenként.
+http://assdfasdfasdfdfís.ddd
+A brit kutatók úgy gyorsították fel a kábelben száguldó fény sebességét, hogy kivették az üveget az üvegszálból. Az így létrehozott, belül üreges, de a fény irányát továbbra is megtartó kábel belsejében azonban még mindig túl nagy a főleg hosszú távon bekövetkező jelveszteség ahhoz, hogy a távközlési vállalatok egymás lábára taposva rohanjanak lecserélni az óceánokban most használt megoldásokat. A 3,5 dB/km jelveszteséggel dolgozó új kábel amúgy 37 párhuzamos csatornán egyszerre adva éri el a másodpercenkénti 1,48 terabites adatátviteli sebességet, és ezzel főleg a rövid távú összeköttetés oldható meg. A sebesség például egy-egy több száz gépet alkalmazó adatközpontban jöhet jól az adatokat tároló és feldolgozó egységek összekötésére.
+http://asasdfasdfasdfasdfasdfdfís.adsf
+A kutatók most azon dolgoznak, hogy a hosszabb távú kapcsolatoknál szükség";
+
+echo Tools::identify_link($text);
 ?>
