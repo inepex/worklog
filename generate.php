25af20ee261e4efd94a7f9286751cf02f4c2e0de
tibor.hidi <tibor.hidi@8fbec55a-2e2f-4785-ad45-5398fcf08caf>
2013-04-30 09:39:29 +0000

    disabled debug
    git-svn-id: https://svn.inepex.com/projects/Worklog@12515 8fbec55a-2e2f-4785-ad45-5398fcf08caf

--- generate.php
+++ generate.php
@@ -20,10 +20,10 @@
 	require_once 'classes/StatusBar.php';
 	require_once 'classes/Tools.php';
 	require_once 'classes/Log.php';
-	require_once 'classes/PhpConsole.php';
-	
 	include('include/login_functions.php');
-	PhpConsole::start();
+	//help for debug
+	//require_once 'classes/PhpConsole.php';
+	//PhpConsole::start();
 	error_reporting(E_ALL);
 	if(isset($_SESSION['enterid'])){
 		$user_id = $_SESSION['enterid'];
