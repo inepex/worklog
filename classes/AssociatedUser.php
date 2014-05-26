b8ef7a1f0f2034f7e7bab353efb04e22dcd86e52
Horváth Gergely J <gergely.horvath@inepex.com>
2014-05-26 16:36:19 +0200

    Cache AssociatedUser.

--- classes/AssociatedUser.php
+++ classes/AssociatedUser.php
@@ -1,32 +1,47 @@
-<?php 
-class AssociatedUser extends User{
+<?php
+
+class AssociatedUser extends User {
 	private $assoc_id;
 	private $project_id;
-	
-	public function __construct($assoc_id){
-		$query = "SELECT * FROM worklog_projects_user_assoc WHERE worklog_projects_user_assoc_id = ".$assoc_id;
-		$select_result = mysql_query($query);
-		$row = mysql_fetch_assoc($select_result);
-		parent::__construct($row['worklog_user_id']);
-		$this->project_id = $row['worklog_project_id'];
-		$this->assoc_id = $assoc_id;
-	}
-	public function is_have_log_in_project(){
-		$query = "SELECT worklog_log_id FROM worklog_log where worklog_user_id = ".$this->id." AND worklog_project_id = ".$this->project_id;
-		$result = mysql_query($query);
-		if(mysql_affected_rows() > 0){
-			return true;
+
+	public function __construct($assoc_id) {
+		$this->objectName = 'AssociatedUser';
+		if ($this->isCached($assoc_id)) {
+			$this->setFromCache($this->getCached($assoc_id));
+		} else {
+			$query = "SELECT * FROM worklog_projects_user_assoc WHERE worklog_projects_user_assoc_id = " . $assoc_id;
+			$select_result = mysql_query($query);
+			$row = mysql_fetch_assoc($select_result);
+			parent::__construct($row['worklog_user_id']);
+			$this->project_id = $row['worklog_project_id'];
+			$this->assoc_id = $assoc_id;
+			$this->cache($assoc_id, $this);
 		}
-		else{
+	}
+
+	public function is_have_log_in_project() {
+		$query = "SELECT worklog_log_id FROM worklog_log where worklog_user_id = " . $this->id . " AND worklog_project_id = " . $this->project_id;
+		$result = mysql_query($query);
+		if (mysql_affected_rows() > 0) {
+			return true;
+		} else {
 			return false;
 		}
 	}
-	public function get_assoc_id(){
+
+	public function get_assoc_id() {
 		return $this->assoc_id;
 	}
-	public function get_project_id(){
+
+	public function get_project_id() {
 		return $this->project_id;
 	}
-}
 
-?>
+	protected function setFromCache($object) {
+		parent::setFromCache($object);
+		$this->assoc_id = $object->assoc_id;
+		$this->project_id = $object->project_id;
+	}
+
+
+}
