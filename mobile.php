a5c5b9dbe141d724b457a6d509623cbf537fb9fb
Hidi Tibor <hidi.tibi@gmail.com>
2014-07-08 13:16:12 +0200

    removed footer

--- mobile.php
+++ mobile.php
@@ -4,7 +4,7 @@ $title = "Home";
 
 $include_list[ 'head' ]		= array();
 $include_list[ 'main' ]	    = array('pages/mobile.inc.php');
-$include_list[ 'foot' ]	    = array('pages/footer.inc.php');
+$include_list[ 'foot' ]	    = array();
 
 include("generate.php");
 ?>
