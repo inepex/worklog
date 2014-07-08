8cadee48aba1d340cede45c51bb96f7083ac81d1
Hidi Tibor <hidi.tibi@gmail.com>
2014-07-08 15:01:11 +0200

    auto with for the container

--- pages/mobile_header.inc.php
+++ pages/mobile_header.inc.php
@@ -1,7 +1,7 @@
 <div class="header"  style="width: auto !important;">
     <div class="navbar navbar-inverse">
         <div class="navbar-inner">
-            <div class="container">
+            <div class="container" style="width: auto !important;">
 
                 <a class="brand" href="index.php" style="color: #ffffff !important;"><img src="images/logo.png" style="margin-right: 5px; margin-top: -6px;">Worklog</a>
             </div>
