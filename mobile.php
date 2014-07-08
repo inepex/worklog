108f45697ce50b96a21f7dc1d7eaa97e8c1c05fe
Hidi Tibor <hidi.tibi@gmail.com>
2014-07-08 13:14:20 +0200

    removed header

--- mobile.php
+++ mobile.php
@@ -2,7 +2,7 @@
 
 $title = "Home";
 
-$include_list[ 'head' ]		= array('pages/header.inc.php');
+$include_list[ 'head' ]		= array();
 $include_list[ 'main' ]	    = array('pages/mobile.inc.php');
 $include_list[ 'foot' ]	    = array('pages/footer.inc.php');
 
