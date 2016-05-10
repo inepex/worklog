281a1d8e28fadce12db9da8f39177889f8921afc
Enzim89 <enzim89@gmail.com>
2016-05-10 13:02:50 +0200

    New Add User page and User edit option

---
+++ add_user.php
@@ -0,0 +1,11 @@
+<?php
+
+$title = "Edit Users";
+
+$include_list[ 'head' ]		= array('pages/header.inc.php');
+$include_list[ 'main' ]	    = array('pages/add_user.inc.php');
+$include_list[ 'foot' ]	    = array('pages/footer.inc.php');
+
+include ("generate.php");
+
+?>
--- pages/footer.inc.php
+++ pages/footer.inc.php
@@ -1,3 +1,3 @@
 <div class="footer">
-Copyright &copy 2013 Inepex Ltd. <a href="?desktop=false">View Mobile</a>
+Copyright &copy 2016 Inepex Ltd. <a href="?desktop=false">View Mobile</a>
 </div>
